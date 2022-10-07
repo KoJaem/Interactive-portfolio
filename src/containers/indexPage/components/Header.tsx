62b008f87c08a32e665beb6272a11e5bd1268d0a
KoJaem <woalswhwh@naver.com>
2022-10-08 05:38:42 +0900

    feat : 헤더 애니메이션 적용

--- src/containers/indexPage/components/Header.tsx
+++ src/containers/indexPage/components/Header.tsx
@@ -1,7 +1,24 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { Typography } from 'src/components';
 import { customColor } from 'src/constants';
 import styled, { css } from 'styled-components';
+
+const buttonVariants = {
+  initial: {
+    color: customColor.darkGray,
+  },
+  hover: {
+    borderBottom: `3px solid ${customColor.gray}`,
+    color: customColor.white,
+    transition: {
+      duration: 0.1
+    },
+  },
+  tap: {
+    borderBottom: `1px solid ${customColor.gray}`,
+  },
+};
 
 export const Header = () => {
   return (
@@ -15,20 +32,36 @@ export const Header = () => {
         </Typography>
       </Title>
       <ButtonWrapper>
-        <Button type="button">
-          <Typography size="20" color="white" fontWeight="bold">
-            My Info
-          </Typography>
+        <Button
+          type="button"
+          initial="initial"
+          variants={buttonVariants}
+          whileHover="hover"
+          whileTap="tap"
+        >
+          My Info
         </Button>
-        <Button type="button">
-          <Typography size="20" color="white" fontWeight="bold">
-            Projects
-          </Typography>
+        <Button
+          type="button"
+          initial="initial"
+          variants={buttonVariants}
+          whileHover="hover"
+          whileTap="tap"
+        >
+          Projects
         </Button>
-        <GithubButton type="button">
-          <Typography size="20" color="black" fontWeight="bold">
-            Github
-          </Typography>
+        <GithubButton
+          type="button"
+          initial={{ opacity: 0.6 }}
+          whileHover={{
+            opacity: 1,
+            color: customColor.white,
+          }}
+          whileTap={{
+            borderRadius: '50%',
+          }}
+        >
+          Github
         </GithubButton>
       </ButtonWrapper>
     </Container>
@@ -52,6 +85,7 @@ const Title = styled.section`
 const ButtonWrapper = styled.section`
   display: flex;
   gap: 0 80px;
+  font-size: 20px;
 `;
 
 const buttonCss = css`
@@ -59,17 +93,21 @@ const buttonCss = css`
   align-items: center;
   justify-content: center;
   width: 120px;
+  height: 40px;
   padding: 12px 0;
   border-radius: 20px;
+  box-sizing: border-box;
+  font-size: 20px;
+  font-weight: bold;
   cursor: pointer;
 `;
 
-const Button = styled.button`
+const Button = styled(motion.button)`
   ${buttonCss}
   background-color: transparent;
 `;
 
-const GithubButton = styled.button`
+const GithubButton = styled(motion.button)`
   ${buttonCss}
   background: linear-gradient(60deg, ${customColor.magenta} 0%, ${customColor.purple} 100%);
 `;
