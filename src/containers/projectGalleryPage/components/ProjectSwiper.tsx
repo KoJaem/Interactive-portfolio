adcc8e29df5322fa2fc5988da8591910b0442d28
KoJaem <woalswhwh@naver.com>
2023-01-06 21:23:16 +0900

    style : swiperJs 커스텀 스타일링중

--- src/containers/projectGalleryPage/components/ProjectSwiper.tsx
+++ src/containers/projectGalleryPage/components/ProjectSwiper.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import styled from 'styled-components';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import styled, { css } from 'styled-components';
 import Image from 'next/image';
 import { projects } from 'src/dummy/projectUrls';
 import { Navigation, Pagination } from 'swiper';
+import { customColor } from 'src/constants';
+import { useRouter } from 'next/router';
 
 export const ProjectSwiper = () => {
   return (
@@ -18,30 +16,25 @@ export const ProjectSwiper = () => {
           loop={true}
           spaceBetween={100}
           pagination={{
+            el: '.swiper_pagination',
             clickable: true,
           }}
-          // navigation={{
-          //   nextEl: '.swiper-button-prev',
-          //   prevEl: '.swiper-button-next',
-          // }}
-          navigation={true}
+          navigation={{
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+          }}
           modules={[Pagination, Navigation]}
-          // breakpoints={{
-          //   1024: {
-          //     coverflowEffect: {
-          //       rotate: 20,
-          //       depth: -80,
-          //     },
-          //   },
-          // }}
         >
+          <PrevButton className="swiper-button-prev">이전</PrevButton>
+          <NextButton className="swiper-button-next">다음</NextButton>
+          <PaginationButton className="swiper_pagination"></PaginationButton>
           {projects.map((data, i) => (
             <StyledSwiperSlide key={i}>
               <Image
                 src={data.url}
                 layout="fill"
                 alt="project"
-                style={{ borderRadius: '24px', overflow: 'hidden' }}
+                style={{ borderRadius: '24px' }}
               />
             </StyledSwiperSlide>
           ))}
@@ -72,13 +65,53 @@ const Wrapper = styled.section`
 const StyledSwiper = styled(Swiper)`
   width: 100%;
   height: 100%;
-  padding: 40px;
+  padding: 20px;
 `;
 
 const StyledSwiperSlide = styled(SwiperSlide)`
   text-align: center;
   display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
   justify-content: center;
   align-items: center;
 `;
 
+const ButtonCss = css`
+  position: absolute;
+  top: '50%';
+  z-index: 999;
+  background-color: ${customColor.white};
+  --swiper-theme-color: green; // Todo 컬러 변경
+  width: 60px;
+  height: 60px;
+  border-radius: 999px;
+  ::after {
+    display: none;
+  }
+`;
+
+const PrevButton = styled.button`
+  ${ButtonCss};
+  left: 0;
+`;
+
+const NextButton = styled.button`
+  ${ButtonCss};
+  right: 0;
+`;
+
+const PaginationButton = styled.div`
+  display: flex;
+  justify-content: center;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 20px;
+  background-color: ${customColor.purple};
+  --swiper-theme-color: ${customColor.skyBlue};
+  .swiper-pagination-bullet {
+    width: 20px;
+    height: 20px;
+  }
+`;
