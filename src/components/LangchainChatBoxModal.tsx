cbe62dba298d1e97be6b7822137f66a150206e88
KoJaem <woalswhwh@naver.com>
2024-02-04 16:31:17 +0900

    chore

--- src/components/LangchainChatBoxModal.tsx
+++ src/components/LangchainChatBoxModal.tsx
@@ -44,6 +44,7 @@ const LangchainChatBoxModal = ({ boxHeaderColor, handleModal }: Props) => {
   const submit = async (data: FieldValues) => {
     try {
       if (!process.env.NEXT_PUBLIC_LAMBDA_URL || invalidAccessKey) {
+        console.log('NEXT_PUBLIC_LAMBDA_URL', process.env.NEXT_PUBLIC_LAMBDA_URL);
         return;
       }
       const question = data.question;
