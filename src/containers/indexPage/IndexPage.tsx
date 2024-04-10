e28978f9523d70c315f6daeb9a4ee0f5b5b38bfd
KoJaem <woalswhwh@naver.com>
2024-04-10 22:48:50 +0900

    feat: PhoneGameComponent 구현 #19

--- src/containers/indexPage/IndexPage.tsx
+++ src/containers/indexPage/IndexPage.tsx
@@ -1,27 +1,41 @@
 import { useRef } from 'react';
 import { customColor } from 'src/constants';
 import styled from 'styled-components';
-import { Header, Intro, MyInfo, MySkills, Projects } from './components';
+import {
+  Header,
+  Intro,
+  MyInfo,
+  MySkills,
+  PhoneGameComponent,
+  Projects,
+} from './components';
 
 export const IndexPage = () => {
   const buttonRefs = useRef<null[] | HTMLElement[]>(new Array(3));
   return (
     <Container>
       <Header refs={buttonRefs} />
-      {/* <Iphone width={700} height={400}>
-        <Iframe
-          width={600}
-          height={300}
-          src="https://d2ql7g3zfmsy1e.cloudfront.net"
-        />
-      </Iphone>
-      <Iphone width={1100} height={530}>
-        <Iframe
-          width={932}
-          height={430}
-          src="https://d3aqn2y3q7oegc.cloudfront.net"
-        />
-      </Iphone> */}
+      {/* <PhoneGameComponent
+        containerWidth={700}
+        containerHeight={400}
+        iframeWidth={600}
+        iframeHeight={300}
+        src="https://d2ql7g3zfmsy1e.cloudfront.net"
+      /> */}
+      <PhoneGameComponent
+        containerWidth={1100}
+        containerHeight={530}
+        iframeWidth={932}
+        iframeHeight={430}
+        src="https://d2ql7g3zfmsy1e.cloudfront.net"
+      />
+      <PhoneGameComponent
+        containerWidth={1100}
+        containerHeight={530}
+        iframeWidth={932}
+        iframeHeight={430}
+        src="https://d3aqn2y3q7oegc.cloudfront.net"
+      />
       <Intro />
       <GapLine />
       <MyInfo refs={buttonRefs} />
@@ -50,36 +64,4 @@ const GapLine = styled.hr`
 
 const Gap = styled.section`
   margin: 20vh 0;
-`;
-
-type StyledProps = {
-  width: number;
-  height: number;
-};
-const Iphone = styled.section<StyledProps>`
-  position: relative;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  width: ${({ width }) => `${width}px`};
-  height: ${({ height }) => `${height}px`};
-  /* width: 710px; */
-  /* height: 400px; */
-  overflow: hidden;
-  background-image: url('iPhone.png');
-  background-repeat: no-repeat;
-  overflow: hidden;
-  object-fit: contain;
-  background-size: contain;
-  background-position: center;
-  display: none;
-  @media screen and (min-width: 1280px) {
-    display: flex;
-  }
-`;
-
-const Iframe = styled.iframe<StyledProps>`
-  width: ${({ width }) => `${width}px`};
-  height: ${({ height }) => `${height}px`};
-  border-radius: 24px;
 `;
